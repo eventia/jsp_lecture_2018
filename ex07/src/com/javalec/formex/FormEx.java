dcafa90a0ce096bf421c1515c6650fcfd4c16789
eventia <eventia@gmail.com>
2018-04-28 12:46:55 +0900

    CH07_UsingForm

---
+++ ex07/src/com/javalec/formex/FormEx.java
@@ -0,0 +1,74 @@
+package com.javalec.formex;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.PrintWriter;
+import java.util.Arrays;
+//import com.javalec.ex.PrintWriter;
+//import com.javalec.ex.String;
+
+/**
+ * Servlet implementation class FormEx
+ */
+@WebServlet("/FormEx")
+public class FormEx extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FormEx() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("doGet1");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		doGet(request, response);
+		System.out.println("doPost2");
+		
+//		request.setCharacterEncoding("UTF-8");
+		request.setCharacterEncoding("EUC-KR");
+		
+		String name = request.getParameter("name");
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		
+		String[] hobbys = request.getParameterValues("hobby");
+		String major = request.getParameter("major");
+		String protocol = request.getParameter("protocol");
+		
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter writer= response.getWriter();
+		
+		writer.println("<html><head></head><body>");
+		writer.println("이름 : " + name + "<br />");
+		writer.println("아이디 : " + id + "<br />");
+		writer.println("비밀번호 : " + pw + "<br />" );
+		writer.println("취미 : " + Arrays.toString(hobbys) + "<br />");
+		writer.println("전공 : " + major + "<br />");
+		writer.println("프로토콜 : " + protocol);
+		writer.println("</body></html>");
+		
+		writer.close();
+		
+	}
+
+}
